64e1eede621e50d4687a7afea33fed7b4a0d7a7b
Nuys <38399240+Nuys@users.noreply.github.com>
2020-07-30 09:09:56 +0300

    Update CaseFieldsListViewController.js

--- CaseFieldsListViewController.js
+++ CaseFieldsListViewController.js
@@ -2,6 +2,7 @@
     Search: function(component, event, helper) {
        var searchText = component.get('v.searchKeyWord');
         var typefield = component.get('v.pickList');
+         console.log(typefield);
          var action = component.get('c.fetchCase');
         action.setParams({searchKeyWord:searchText, typeField: typefield} );
         action.setCallback(this, function(response) {
@@ -10,19 +11,13 @@
             var labels;
             if (state === 'SUCCESS') {
                 cases = response.getReturnValue().objects;
-                labels = response.getReturnValue().labels;
+                labels = response.getReturnValue().listOfMaps;
                 console.log(cases);
+                
             }
             component.set("v.data", cases);
-            if(typefield !== 'CaseNumber'){
-            component.set('v.mycolumns',[                
-                {label: 'Case Number', fieldName: 'CaseNumber', type: 'text'},
-                {label: labels[typefield], fieldName: typefield, type: 'text'}] )
-                }
-            else{
-                component.set('v.mycolumns',[                
-                {label: 'Case Number', fieldName: 'CaseNumber', type: 'text'}])
-            }
+            
+            component.set('v.mycolumns', labels);
         });
         
         $A.enqueueAction(action);
