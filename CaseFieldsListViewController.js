b259a443b28003806c31b7b6439dfc2049ee1203
Nuys <38399240+Nuys@users.noreply.github.com>
2020-07-31 18:47:25 +0300

    Update CaseFieldsListViewController.js

--- CaseFieldsListViewController.js
+++ CaseFieldsListViewController.js
@@ -1,39 +1,22 @@
 ({
     Search: function(component, event, helper) {
-       let button = event.getSource();
-      
-   		 button.set('v.disabled',true);
-       var searchText = component.get('v.searchKeyWord');
-        if(searchText.trim() == 0){
-            button.set('v.disabled',true);
-        }else{
-        var typefield = component.get('v.pickList');
-         console.log(typefield);
-         var action = component.get('c.fetchCase');
-        action.setParams({searchKeyWord:searchText, typeField: typefield} );
-        action.setCallback(this, function(response) {
-            var state = response.getState();
-            var cases;
-            var labels;
-            if (state === 'SUCCESS') {
-                cases = response.getReturnValue().objects;
-                labels = response.getReturnValue().listOfMaps;
-                cases.forEach(function(field){
-                    field.CaseLink = '/' + field.Id;
-                    });
-            }
-            component.set("v.data", cases);
-            component.set('v.mycolumns', labels);
-            
-        });
-        
-        $A.enqueueAction(action);
-        }
+        helper.SearchCase(component, event); 
     },
-      activeButton : function(component, event, helper){
+    activeButton : function(component, event, helper){
         let inputText = component.find("searchField").get("v.value");
         if(inputText != null){
             component.set('v.isButtonActive',false);
         }       
     },
+        handleNext : function(component, event, helper) { 
+        var pageNumber = component.get("v.pageNumber");
+        component.set("v.pageNumber", pageNumber+1);
+        helper.SearchCase(component, event); //not work 
+        },
+     handlePrev : function(component, event, helper) {        
+        var pageNumber = component.get("v.pageNumber");
+        component.set("v.pageNumber", pageNumber-1);
+         helper.SearchCase(component, event);
+    },
+    
 })
