005b965d55871d5fd7d93937d7388ae05ecdc1b1
Nuys <38399240+Nuys@users.noreply.github.com>
2020-08-04 14:25:47 +0300

    Update CaseFieldsListViewController.js

--- CaseFieldsListViewController.js
+++ CaseFieldsListViewController.js
@@ -2,49 +2,49 @@
     Search: function(component, event, helper) {
         helper.SearchCase(component, event);   
         helper.countPages(component, event);
-       
+        
         
     },
     activeButton : function(component, event, helper){
-       
+        
         
         var test = component.get("v.searchKeyWord");
         console.log(test.trim());
         if(!test.trim()){
-         
+           //component.set('v.') 
             component.set('v.isLastPage',true);
             component.set('v.pageNumber', 1);
+            component.set('v.searchButton', true);
             
         }else{
-             component.set('v.isLastPage',false);
-           
-        }     
+            component.set('v.searchButton', false);
+        }    
     },
     handleNext : function(component, event, helper) { 
         
-	var isDisabled = component.find('nextButton'); 
+        var isDisabled = component.find('nextButton'); 
         
         var countPages = component.get("v.countPages");
         var pageNumber = component.get("v.pageNumber");
-       
-      
+        
+        
         component.set("v.pageNumber", pageNumber+1);
-      	
+        
         if(pageNumber + 1 == countPages ){
             component.set("v.isLastPage", true);
         }
-       
+        
         helper.SearchCase(component, event); 
-		
+        
     },
     handlePrev : function(component, event, helper) {  
-          
+        
         var countPages = component.get("v.countPages");
-         var pageNumber = component.get("v.pageNumber");
-       
+        var pageNumber = component.get("v.pageNumber");
+        
         component.set("v.pageNumber", pageNumber-1);
         pageNumber = component.get("v.pageNumber");
-      
+        
         if(pageNumber < countPages ){
             component.set("v.isLastPage", false);
             
